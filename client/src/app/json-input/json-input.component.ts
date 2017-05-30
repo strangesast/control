0a8aee34599f5c0af79f7032f281480e8681c14e
Sam Zagrobelny <strangesast@gmail.com>
2017-05-30 15:54:36 -0400

    added default option

--- client/src/app/json-input/json-input.component.ts
+++ client/src/app/json-input/json-input.component.ts
@@ -72,4 +72,182 @@ export class JsonInputComponent {
       this.json = template.template;
     }
   }
+
+  loadDefault() {
+    this.json = {
+      "components": {
+        "root": {
+          "type": "ListGroupComponent",
+          "attributes": [
+            {
+              "name": "backgroundColor",
+              "type": "string",
+              "value": "#eee"
+            },
+            {
+              "name": "children",
+              "type": "array",
+              "value": [
+                "group0",
+                "group1",
+                "group2",
+                "group3"
+              ]
+            }
+          ]
+        },
+        "group0": {
+          "type": "GroupComponent",
+          "attributes": [
+            {
+              "name": "name",
+              "type": "string",
+              "value": "Group 1"
+            },
+            {
+              "name": "children",
+              "type": "array",
+              "value": [
+                "numericInput0",
+                "numericInput1"
+              ]
+            }
+          ]
+        },
+        "group1": {
+          "type": "GroupComponent",
+          "attributes": [
+            {
+              "name": "name",
+              "type": "string",
+              "value": "Group 2"
+            },
+            {
+              "name": "children",
+              "type": "array",
+              "value": [
+                "gaugeComponent0",
+                "graphComponent0",
+                "numericInput1"
+              ]
+            }
+          ]
+        },
+        "group2": {
+          "type": "GroupComponent",
+          "attributes": [
+            {
+              "name": "name",
+              "type": "string",
+              "value": "Group 3"
+            },
+            {
+              "name": "children",
+              "type": "array",
+              "value": [
+                "gaugeComponent0",
+                "numericInput1"
+              ]
+            }
+          ]
+        },
+        "group3": {
+          "type": "GroupComponent",
+          "attributes": [
+            {
+              "name": "name",
+              "type": "string",
+              "value": "Group 4"
+            }
+          ]
+        },
+        "numericInput0": {
+          "type": "NumericInputComponent",
+          "attributes": [
+            {
+              "name": "label",
+              "type": "string",
+              "value": "Temperature in Room B"
+            },
+            {
+              "name": "value",
+              "type": "number",
+              "id": "temperature"
+            },
+            {
+              "name": "color",
+              "type": "string",
+              "value": "purple"
+            },
+            {
+              "name": "backgroundColor",
+              "type": "string",
+              "value": "white",
+              "write": true
+            }
+          ]
+        },
+        "numericInput1": {
+          "type": "NumericInputComponent",
+          "attributes": [
+            {
+              "name": "label",
+              "type": "string",
+              "value": "Set Point 1"
+            },
+            {
+              "name": "value",
+              "type": "number",
+              "id": "setPoint"
+            },
+            {
+              "name": "color",
+              "type": "string",
+              "value": "purple"
+            },
+            {
+              "name": "backgroundColor",
+              "type": "string",
+              "value": "white"
+            },
+            {
+              "name": "write",
+              "type": "boolean",
+              "value": true
+            }
+          ]
+        },
+        "graphComponent0": {
+          "type": "GraphComponent",
+          "attributes": [
+            {
+              "name": "value",
+              "type": "number",
+              "id": "temperature"
+            }
+          ]
+        },
+        "gaugeComponent0": {
+          "type": "ThermostatComponent",
+          "attributes": [
+            {
+              "name": "value",
+              "type": "number",
+              "id": "setPoint"
+            }
+          ]
+        }
+      },
+      "values": {
+        "temperature": {
+          "type": "number",
+          "name": "Temperature Monitor 1"
+        },
+        "setPoint": {
+          "type": "number",
+          "name": "Set Point 1"
+        }
+      }
+    }
+  }
 }
