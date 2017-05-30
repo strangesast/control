3ad86b9de1d7746199e596f9e608ef60e92bc287
Sam Zagrobelny <strangesast@gmail.com>
2017-05-30 14:07:24 -0400

    added save-able templates

--- client/src/app/json-input/json-input.component.ts
+++ client/src/app/json-input/json-input.component.ts
@@ -9,6 +9,7 @@ import { RegistrationService } from '../registration.service';
 export class JsonInputComponent {
   jsonValue;
   text: string;
+  templates: any[];
   public valid: boolean = true;
   @Output() jsonChange = new EventEmitter();
   @Input() get json(): any {
@@ -25,6 +26,11 @@ export class JsonInputComponent {
   constructor(private registration: RegistrationService) { }
 
   ngOnInit() {
+    try {
+      this.templates = JSON.parse(localStorage.getItem('templates')) || [];
+    } catch (e) {
+      this.templates = [];
+    }
     this.registration.registeredTemplate.subscribe(template => {
       this.json = template;
     });
@@ -36,6 +42,34 @@ export class JsonInputComponent {
       this.valid = true;
     } catch (e) {
       this.valid = false;
+    }
+  }
+
+  saveTemplate(name) {
+    if (this.valid) {
+      let template = this.json;
+      let existing = this.templates.find(({name: _name}) => name == _name);
+      if (existing) {
+        existing.template = template;
+      } else {
+        this.templates.push({ name, template });
+      }
+      localStorage.setItem('templates', JSON.stringify(this.templates))
+    }
+  }
+
+  removeTemplate(index) {
+    let template = this.templates[index];
+    if (template) {
+      this.templates.splice(index, 1);
+      localStorage.setItem('templates', JSON.stringify(this.templates));
+    }
+  }
+
+  loadTemplate(index) {
+    let template = this.templates[index];
+    if (template) {
+      this.json = template.template;
     }
   }
 }
