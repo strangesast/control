e8a72641aac05bec5ff6501bf6ed6f289957f240
Sam Zagrobelny <strangesast@gmail.com>
2017-08-16 18:24:19 -0400

    added test for floors per building

--- server/spec-node/duplicates.spec.js
+++ server/spec-node/duplicates.spec.js
@@ -19,6 +19,7 @@ describe('duplicates', async () => {
 
     await importFromGeo(mongo, path.join(__dirname, '../../data/geo'));
     await duplicateFloors(mongo, 2);
+    await duplicateBuildings(mongo, 2);
 
     let floors = await mongo.collection('areas').aggregate([
       { $group: { _id: '$floor' }},
@@ -26,10 +27,28 @@ describe('duplicates', async () => {
       { $unwind: '$object' }
     ]).toArray();
 
-    expect(floors).to.have.lengthOf(3);
+    // 2 more floors for each of 2 more buildings
+    floors.should.have.lengthOf(6);
 
     let roomIds = await mongo.collection('points').distinct('room');
     let roomCnt = await mongo.collection('areas').count({ type: 'room' });
-    expect(roomIds).to.have.lengthOf(roomCnt);
+    roomIds.should.have.lengthOf(roomCnt);
+
+    let buildingsPerFloorShortname = await mongo.collection('areas').aggregate([
+        { $match: { type: 'floor' }},
+        { $lookup: {
+              from: 'areas',
+              localField: '_id',
+              foreignField: 'floor',
+              as: 'values'
+            }},
+        { $unwind: '$values' },
+        { $group: { _id: { shortname: '$shortname', building: '$values.building' }, values: { $push: '$_id' }}},
+        { $unwind: '$values' },
+        { $group: { _id: '$_id.shortname', values: { $addToSet: '$_id.building' }}},
+        { $project: { length: { $size: '$values' }}}
+    ]).toArray();
+
+    buildingsPerFloorShortname.should.all.have.property('length', 2);
   });
 });
